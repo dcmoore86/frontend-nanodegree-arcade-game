dfb88b59313ef591344c6cca42da33493dd64060
dcmoore86 <mooremoat@gmail.com>
2018-05-06 20:06:37 -0400

    removing default vars used for declarations

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -119,7 +119,7 @@ for (let i = 0; i < 3; i++) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
